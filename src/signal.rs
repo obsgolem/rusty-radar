72c30b0af62951e44695bc632b24c6e3e9669433
Josiah Bills <josiah@adoniram.net>
2021-09-26 22:38:29 -0400

    Added more documentation to the signal library.

--- src/signal.rs
+++ src/signal.rs
@@ -1,3 +1,15 @@
+/*!
+Contains high and low fidelity algorithms for working with signals.
+
+This library provides tools for working with continuous time signals through the [Signal] trait, together with a number of built in signal types.
+
+It also provides a number of tools for working with discrete time signals. [SampledDomain] represents a time or frequency domain that has been sampled at a frequency.
+[convolve] and friends allow you to apply fir filters. The [fir] module provides a number of windows for filter design, together with a low pass filter [fir::sinc_lowpass].
+The [fft] module provides a trait which makes working with fourier transforms of [ndarray::Array1] a bit easier. The [iir] module will eventually provide tools for making and applying iir filters.
+
+High fidelity functions for quadrature modulation and demodulation are available through [qam_modulate] and [qam_demodulate].
+*/
+
 use std::{
     borrow::BorrowMut,
     cell::RefCell,
@@ -19,10 +31,11 @@ pub mod fir;
 pub mod iir;
 pub mod scalar;
 
-pub fn sampling_freq_to_len(interval: f32, sampling_frequency: f32) -> usize {
+fn sampling_freq_to_len(interval: f32, sampling_frequency: f32) -> usize {
     (interval * sampling_frequency).round() as usize
 }
 
+/// Performs the operation `sin(x)/x`
 pub fn sinc<F: Float>(x: F) -> F {
     if x == F::zero() {
         F::one()
@@ -31,6 +44,7 @@ pub fn sinc<F: Float>(x: F) -> F {
     }
 }
 
+/// Performs the operation `sin(πx)/πx`
 pub fn nsinc<F: Float + FloatConst>(x: F) -> F {
     let x = x * F::PI();
     if x == F::zero() {
@@ -48,6 +62,11 @@ fn rectangle(t: f32, width: f32) -> f32 {
     }
 }
 
+/// A trait providing an abstract representation of a signal.
+///
+/// An implementor of this trait represents a continuous-time signal. The trait provides a uniform interface for sampling such signals.
+/// Implementing this trait does not require determinism, see [GaussianNoise].
+/// If your [Signal::generate] method has a large overhead which is needed only once for multiple samples, consider implementing [Signal::generate_signal] in addition to [Signal::generate].
 pub trait Signal {
     type Valued: Scalar;
     fn generate(&self, t: f32) -> Self::Valued;
@@ -57,7 +76,12 @@ pub trait Signal {
     }
 }
 
-// Represents a time or frequency interval, sampled at a given rate or interval.
+/**
+Represents a sampled time or frequency range.
+
+This captures all the relevant properties of a range including the start and end of the range, the sampling frequency, the interval between samples, and the total number of samples.
+The samples can be iterated over using [`SampledDomain::iter`] and can be converted to an array of samples using `into`.
+*/
 #[derive(Clone)]
 pub struct SampledDomain {
     start: f32,
@@ -68,6 +92,7 @@ pub struct SampledDomain {
 }
 
 impl SampledDomain {
+    /// Constructs a time or frequency range using the given sampling frequency.
     pub fn new(start: f32, end: f32, freq: f32) -> SampledDomain {
         assert!(start <= end);
         let len = sampling_freq_to_len(end - start, freq);
@@ -80,6 +105,7 @@ impl SampledDomain {
         }
     }
 
+    /// Constructs a time or frequency range using the given number of evenly spaced samples.
     pub fn from_sample_count(start: f32, end: f32, num: usize) -> SampledDomain {
         assert!(start <= end);
         let freq = num as f32 / (end - start);
@@ -92,6 +118,7 @@ impl SampledDomain {
         }
     }
 
+    /// Constructs a time or frequency range using the given interval between samples and a given number of samples.
     pub fn from_sample_interval(start: f32, interval: f32, num: usize) -> SampledDomain {
         let freq = 1. / interval;
         let end = start + (num as f32 * interval);
@@ -104,23 +131,27 @@ impl SampledDomain {
         }
     }
 
+    /// Returns the start time/frequency of the domain.
     pub fn start(&self) -> f32 {
         self.start
     }
 
+    /// Returns the end time/frequency of the domain,
     pub fn end(&self) -> f32 {
         self.end
     }
 
+    /// Returns the sampling frequency of the domain.
     pub fn freq(&self) -> f32 {
         self.freq
     }
 
+    /// Returns the number of samples in the domain.
     pub fn sample_count(&self) -> usize {
         self.samples
     }
 
-    /// The interval (in time or frequency) between individual samples
+    /// The interval (in time or frequency) between individual samples.
     pub fn sample_interval(&self) -> f32 {
         self.interval
     }
@@ -134,6 +165,7 @@ impl SampledDomain {
         linspace(self.start, self.end, self.samples)
     }
 
+    /// Converts the time domain to the frequency domain and vice versa. If you need to plot this you might want to consider [fft_frequencies_plottable] instead.
     pub fn fft_frequencies(&self) -> SampledDomain {
         Self::from_sample_interval(0., 1. / (self.range()), self.samples)
     }
@@ -156,6 +188,9 @@ impl From<SampledDomain> for Array1<f32> {
     }
 }
 
+/// Represents a sine wave of unit amplitude and a given frequency.
+///
+/// A sine wave can also have a phase shift given in radians. A wave of a given frequency can be constructed easily using `f32::into`.
 #[derive(Clone)]
 pub struct Sine {
     pub freq: f32,
@@ -178,6 +213,18 @@ impl From<f32> for Sine {
     }
 }
 
+/**
+Represents a (potentially frequency-modulated) pulse.
+
+A pulse can be either a rectangular pulse or a linear frequency sweep called a chirp.
+If the clock on the pulse has a non-zero frequency, then it represents the carrier wave on which to modulate the baseband pulse signal.
+This will be done by multiplying the baseband pulse by the carrier frequency.
+
+This struct provides a lo-fidelity way of modeling Quadrature Amplitude Modulation (QAM).
+By using this struct, you can abstract away the complex signal processing that needs to happen to modulate and demodulate a signal from the carrier frequency.
+To modulate, simply set the clock to a non-zero frequency. To demodulate, simply set it back to zero. This gives better results than are physically realizable,
+but should be sufficient for a lo-fidelity simulation. For higher fidelity, see [qam_modulate] and [qam_demodulate].
+*/
 #[derive(Clone)]
 pub struct Pulse {
     pub clock: Sine,
@@ -237,15 +284,19 @@ impl Pulse {
     }
 }
 
+/// Represents the type baseband signal of a [Pulse].
 #[derive(Clone)]
 pub enum PulseType {
+    /// A rectangular wave, which is a constant 1 on the interval of the pulse.
     Rectangle,
-    // Chirp, argument is the chirp-to frequency.
+    /// A chirp pulse, which starts at frequency 0 then increases in frequency to the given frequency.
     Chirp(f32),
 }
 
-// Implements a random gaussian signal. This signal is inherently non-deterministic, and calling generate twice with the same value
-// will almost certainly yield different results.
+/// Implements a random gaussian signal.
+///
+/// This signal is inherently non-deterministic, and calling [GaussianNoise::generate] twice with the same value
+/// will almost certainly yield different results.
 pub struct GaussianNoise<T: Rng> {
     rng: RefCell<T>,
     distr: Normal<f32>,
@@ -276,7 +327,7 @@ impl<'a, T: Rng> Signal for GaussianNoise<T> {
     }
 }
 
-// Represents a sinc wave/filter with a given bandwidth.
+/// Represents a sinc wave/filter with a given bandwidth.
 pub struct Sinc {
     pub bandwidth: f32,
 }
@@ -289,8 +340,11 @@ impl Signal for Sinc {
     }
 }
 
+/// A sawtooth wave.
+///
+/// This wave starts at a given amplitude, decreases to negative amplitude over a period, then jumps to the start.
 #[derive(Clone)]
-struct Sawtooth {
+pub struct Sawtooth {
     pub period: f32,
     pub amplitude: f32,
 }
@@ -303,7 +357,7 @@ impl Signal for Sawtooth {
     }
 }
 
-// Ultra-slow naive convolution function
+/// Ultra-slow naive convolution function
 pub fn convolve<T: Scalar>(x: &Array1<T>, y: &Array1<T>) -> Array1<T> {
     let mut z = x.clone();
 
@@ -322,26 +376,41 @@ pub fn convolve<T: Scalar>(x: &Array1<T>, y: &Array1<T>) -> Array1<T> {
     z
 }
 
+/// Convolve two real arrays using ffts.
 pub fn convolve_f32(x: &Array1<f32>, y: &Array1<f32>) -> Array1<f32> {
     (x.clone().fft() * y.clone().fft())
         .ifft_plottable()
         .mapv(|x| x.re())
 }
 
+/// Convolve two complex arrays using ffts.
 pub fn convolve_complex32(x: &Array1<Complex32>, y: &Array1<Complex32>) -> Array1<Complex32> {
     (x.clone().fft() * y.clone().fft()).ifft_plottable()
 }
 
-/// Modulates a baseband signal onto a carrier wave retrieved from the clock. The given time domain should be the time domain of the baseband signal.
-/// This functions by multiplying the baseband signal by an exponential oscillating at the carrier frequency, then taking the real part of that signal.
-/// This is useful because only real signals are physically realizable by e.g. an EM wave.
+/**
+Modulates a baseband signal onto a carrier wave retrieved from the clock.
+
+The given time domain should be the time domain of the baseband signal.
+This functions by multiplying the baseband signal by an exponential oscillating at the carrier frequency, then taking the real part of that signal.
+This is useful because only real signals are physically realizable by e.g. an EM wave.
+
+```rust
+let time = SampledDomain::new(0., 1., 300.);
+let clock: Sine = 100.0.into();
+let signal = tukey(time.sample_count(), 0.3);
+qam_modulate(signal.mapv(|x| x.into()), time, clock);
+```
+*/
 pub fn qam_modulate(signal: Array1<Complex32>, time: SampledDomain, clock: Sine) -> Array1<f32> {
     assert!(signal.len() == time.sample_count(), "Samples in the signal being modulated must match the number of samples represented by the time domain.");
     (signal * clock.generate_signal(&time)).mapv(|x| x.re)
 }
 
 /**
-This function inverts [qam_modulate]. In order to demodulate you need to remove the high frequency carrier signal and retreive the baseband waveform.
+Inverse of [qam_modulate].
+
+In order to demodulate you need to remove the high frequency carrier signal and retreive the baseband waveform.
 We do this by mixing the signal separately with the real and complex part of the original carrier wave.
 If you look at this in the frequency domain then this will result in an extra copy of the signal appearing at `+-2f_0` where `f_0` is the carrier frequency.
 We can apply a so called low-pass filter to remove all frequency components that are above the (one sided) bandwidth of the original complex signal.
@@ -349,8 +418,8 @@ In order for this to work well, the baseband signal should have bandwidth that i
 Note that the bandwidth input on this function need only be a reasonable estimate of the actual bandwidth.
 
 Let `f_0` be the clock frequency. Let `B` be the (one sided) bandwidth of the transmitted signal.
-Let `B' be the bandwidth of the low pass filter used for demodulation. Let `f_s` be the sampling frequency. Then in order for this function to work
-the following relation must hold: `f_s > 2f_0+B+B'. We check a simplified version of this assumption via assertion
+Let `B'` be the bandwidth of the low pass filter used for demodulation. Let `f_s` be the sampling frequency. Then in order for this function to work
+the following relation must hold: `f_s > 2f_0+B+B'`. We check a simplified version of this assumption via assertion
 */
 pub fn qam_demodulate(
     signal: Array1<f32>,
@@ -403,6 +472,7 @@ mod test {
 
         assert_relative_eq!(domain.start(), domain2.start());
         assert_relative_eq!(domain.end(), domain2.end());
+
         assert_relative_eq!(domain.freq(), domain2.freq());
     }
 
